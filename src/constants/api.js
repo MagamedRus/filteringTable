64c993a79896696eac63b5d449898520082b7a91
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-07-13 19:07:21 +0500

    add requests to back

---
+++ src/constants/api.js
@@ -0,0 +1,3 @@
+export const URL_SERVER = "http://localhost:8000";
+export const URL_MAPPOINTS = "/api/mappoints";
+export const URL_DELETE_ALL = '/api/delete/all'
---
+++ src/controllers/mappoints.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import { URL_DELETE_ALL, URL_SERVER, URL_MAPPOINTS } from "../constants/api.js";
+
+/** get map points */
+export const getAllPoints = async () => {
+  let result = [];
+
+  await axios
+    .get(URL_SERVER + URL_MAPPOINTS, {
+      headers: {
+        "Cache-Control": "no-cache", // without it, we have have 304 status on req
+      },
+      params: { maxUsers: 200 },
+    })
+    .then((res) => {
+      const data = res.data;
+      if (data?.points) {
+        result = data.points;
+      }
+    })
+    .catch((err) => console.log("getAllPoints err -", err));
+
+  return result;
+};
+
+/** create new row of points */
+export const createNewMapPoint = async ({ name, distance, count }) => {
+  const currDate = new Date();
+  const date = `${currDate.getFullYear()}-${currDate.getMonth()}-${currDate.getDate()}`;
+  let result = false;
+  const data = {
+    name,
+    distance,
+    count,
+    date,
+  };
+
+  await axios
+    .post(URL_SERVER + URL_MAPPOINTS, data)
+    .then((res) => {
+      const body = res.body;
+      if (body?.isSuccess) {
+        result = true;
+      }
+    })
+    .catch((err) => console.log("createNewMapPoint err -", err));
+
+  return result;
+};
+
+/** delete all map points  */
+export const deleteAllPoints = async () => {
+  let result = false;
+  await axios
+    .delete(URL_SERVER + URL_DELETE_ALL)
+    .then((res) => {
+      const data = res.data;
+      if (data?.isSuccess) {
+        result = true;
+      }
+    })
+    .catch((err) => console.log("deleteAllPoints err -", err));
+
+  return result;
+};
