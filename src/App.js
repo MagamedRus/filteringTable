d890878d74cb1aca849797f3334b597443f57eec
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-07-13 19:17:54 +0500

    add show table with req

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Table from "./components/Table/Table.js";
+import { getAllPoints } from "./controllers/mappoints.js";
 
 function App(props) {
+  const [rowsData, setRowsData] = useState([]);
+
+  useEffect(() => {
+    (async function () {
+      const allPoints = await getAllPoints();
+      allPoints[0] && setRowsData(allPoints);
+      console.log(allPoints)
+    })();
+  }, []);
+
   return (
     <div>
-      asd
+      <Table rows={rowsData} />
     </div>
   );
 }
