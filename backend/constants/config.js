441c1237dd8578bb5ed8b5465987a352b41c8cca
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-07-15 04:26:35 +0500

    change cfg of db

--- backend/constants/config.js
+++ backend/constants/config.js
@@ -1,10 +1,21 @@
 export const PORT = process.env.PORT || 8000;
 
+//For locale db
+// export const MYSQL_CONFIG = {
+//   host: process.env.DB_HOST || 'localhost',
+//   user: 'root',
+//   database: process.env.DB_NAME || 'filteringtable',
+//   password: process.env.DB_PASSWORD || 'root',
+//   waitForConnections: true,
+//   connectionLimit: 200,
+//   queueLimit: 0,
+// };
+
 export const MYSQL_CONFIG = {
-  host: process.env.DB_HOST || 'localhost',
-  user: 'root',
-  database: process.env.DB_NAME || 'filteringtable',
-  password: process.env.DB_PASSWORD || 'root',
+  host: process.env.DB_HOST || 'sql11.freesqldatabase.com',
+  user: 'sql11505823',
+  database: process.env.DB_NAME || 'sql11505823',
+  password: process.env.DB_PASSWORD || 'fZTidrHllk',
   waitForConnections: true,
   connectionLimit: 200,
   queueLimit: 0,
