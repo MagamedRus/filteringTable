38b6072a5904271df9af03e9f97be33b496c356e
Muhammad Kasymov <muhammadkasymov@yandex.com>
2022-07-14 22:39:35 +0500

    deleted Table scss file

--- src/components/TableBody/TableBody.js
+++ src/components/TableBody/TableBody.js
@@ -1,15 +1,14 @@
 import React from "react";
 import RowTable from "../RowTable/RowTable.js";
-import styles from "./TableBody.module.scss";
 
 const TableBody = ({ rows }) => {
   const composeDate = (date) => {
-    const dateArr = date.substr(0, 10).split('-')
+    const dateArr = date.substr(0, 10).split("-");
     return `${dateArr[2]}-${dateArr[1]}-${dateArr[0]}`;
   };
 
   return (
-    <tbody className={styles.container}>
+    <tbody>
       {rows.map((el) => (
         <RowTable
           key={el.id}
